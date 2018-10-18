f2901da6c28da19127929e89089b3273df2adee6
Adam Chen Wei <achenwei@fll1006558l.chewy.local>
2018-10-18 14:59:31 -0400

    broke(storyshot): just can't get the setup right

--- src/stories/index.js
+++ src/stories/index.js
@@ -4,9 +4,14 @@ import { storiesOf } from '@storybook/vue';
 
 import HelloWorld from '../components/HelloWorld.vue';
 
+import initStoryshots from '@storybook/addon-storyshots';
+ 
+initStoryshots();
+
+
 storiesOf('HelloWorld', module)
   //.add('story as a template', () => '<my-button :rounded="true">story as a function template</my-button>')
   .add('story as a component', () => ({
     components: { HelloWorld },
-    template: '<HelloWorld :rounded="true">rounded</HelloWorld>'
+    template: '<HelloWorld :rounded="true"/>'
   }));
